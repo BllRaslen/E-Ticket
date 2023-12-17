a8b502ddbc34f85a7b0be5322d7d415b14706a63
Bilal RASLEN <eng.bll.raslen@gmail.com>
2023-12-17 21:43:12 +0300

    add Models

---
+++ web_1/Models/Sefer.cs
@@ -0,0 +1,37 @@
+﻿using EfCore2C.Models.airline.Models;
+
+namespace EfCore2C.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+
+    public class Sefer
+    {
+        [Key]
+        public int sefer_id { get; set; }
+        public string kalkis_saati { get; set; }
+        public string varis_saati { get; set; }
+        public double sefer_fiati { get; set; }
+
+        
+        public ICollection<Rezervasyon> Rezervasyons { get; set; }
+
+
+        [ForeignKey("KalkisHavalimani")]
+        public int kalkis_havalimani_id { get; set; }
+        public Havalimani KalkisHavalimani { get; set; }
+
+        [ForeignKey("VarisHavalimani")]
+        public int varis_havalimani_id { get; set; }
+        public Havalimani VarisHavalimani { get; set; }
+
+
+
+        [ForeignKey("Firma")]
+        public int firma_id { get; set; }
+        public Firma Firma { get; set; }
+
+
+    }
+}
---
+++ web_1/Models/Sehir.cs
@@ -0,0 +1,16 @@
+﻿namespace EfCore2C.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+
+    public class Sehir
+    {
+        [Key]
+        public int sehir_id { get; set; }
+        public String sehir_adi { get; set; }
+
+        //public List<Havalimani> Havalimanis { get; set; } = new List<Havalimani>();
+        public List<Havalimani> Havalimanis { get; set; } = new List<Havalimani>();
+    }
+}
+ 

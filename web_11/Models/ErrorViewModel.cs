3b18ef0cbefb5d7979f5444020f64deef9c39607
= <eng.bll.raslen@gmail.com>
2023-12-18 21:04:43 +0300

    Remove web_11 folder

--- web_11/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace web_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-} 

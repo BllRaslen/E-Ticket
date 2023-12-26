ee2fb56337e3cbe2feddeb14601e17d5b828a167
m-ali-bathich <0047mohamad@gmail.com>
2023-12-26 16:17:21 +0300

    Updated UI

--- web_1/Models/Sehir.cs
+++ web_1/Models/Sehir.cs
@@ -6,7 +6,9 @@
     public class Sehir
     {
         [Key]
+        [Display(Name = "Sehir_SehirId")]
         public int sehir_id { get; set; }
+        [Display(Name = "Sehir_SehirAdi")]
         public String sehir_adi { get; set; }
 
         //public List<Havalimani> Havalimanis { get; set; } = new List<Havalimani>();
